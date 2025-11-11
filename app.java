2ba15bc07aaaf045b103af50ccfde35c09a042b8
root <root@ip-172-31-81-91.ec2.internal>
2025-11-11 05:03:24 +0000

    added some integer code

--- app.java
+++ app.java
@@ -4,3 +4,23 @@ public class HelloWorld {
         System.out.println("Hello, World!"); 
     }
 }
+
+//Adding some other code 
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
+    }
+} 
